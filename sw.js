48b4bb1d0b8c98616f1e6c3b16763dfb2ef43360
jungwon927 <73385488+jungwon927@users.noreply.github.com>
2026-08-29 05:31:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// 오프라인에서도 열리도록 앱 파일을 캐시에 담아 둡니다.
+var CACHE = "parking-v1";
+var SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(SHELL); }).then(function () {
+    return self.skipWaiting();
+  }));
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) return hit;
+      return fetch(e.request).then(function (res) {
+        if (res && res.ok && new URL(e.request.url).origin === self.location.origin) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
